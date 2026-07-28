60eff9606d914a24c577f17f9cdd26caac1d2ea2
Jamshid Hamza <jamshihamza@gmail.com>
2026-07-28 16:00:33 +0530

    Add LogLevel enum

---
+++ Common/include/optier/LogLevel.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace optier
+{
+    enum class LogLevel
+    {
+        Trace = 0,
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Critical,
+        Off
+    };
+}
