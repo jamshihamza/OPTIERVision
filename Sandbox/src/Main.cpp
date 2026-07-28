84dc6fea9363ac9bf58c8a5c1543893f754850c5
Jamshid Hamza <jamshihamza@gmail.com>
2026-07-28 23:16:21 +0530

    Complete application bootstrap and sandbox

---
+++ Sandbox/src/Main.cpp
@@ -0,0 +1,16 @@
+#include <optier/Application.h>
+
+int main()
+{
+    optier::Application application;
+
+    if (!application.Initialize())
+    {
+        return -1;
+    }
+
+    application.Run();
+    application.Shutdown();
+
+    return 0;
+}
